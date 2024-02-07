30c19faf95d86eb2c6ea09f2ff3e5a61b0c00ad4
long bui <blong1102@gmail.com>
2024-02-07 15:49:36 +0700

    feat: init tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const Page = (): React.ReactElement => {
-  return <div>Hello</div>
+  return (
+    <div>
+      <p className='text-main text-3xl font-bold underline'>Hello</p>
+      <p className='text-secondary'>World</p>
+    </div>
+  )
 }
 
 export default Page
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,9 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-      }
+    colors: {
+      main: '#437254',
+      secondary: '#ffe34d'
     }
   },
   plugins: []
