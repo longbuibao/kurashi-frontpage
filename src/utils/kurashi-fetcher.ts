6ae6d99b9ab10ea2666f3e6398030f12cf6260f9
long bui <blong1102@gmail.com>
2024-03-05 21:33:21 +0700

    #3 fix: remove cache fetcher

--- src/utils/kurashi-fetcher.ts
+++ src/utils/kurashi-fetcher.ts
@@ -1,1 +1,1 @@
-export const kurashiFetcher = async (url: string): Promise<any> => await fetch(url).then(async (res) => await res.json())
+export const kurashiFetcher = async (url: string): Promise<any> => await fetch(url, { cache: 'no-cache' }).then(async (res) => await res.json())
