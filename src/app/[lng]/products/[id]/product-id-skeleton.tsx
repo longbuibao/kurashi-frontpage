1ac76a5a0232a4b8e05631da63a8a01829cf158b
long bui <blong1102@gmail.com>
2024-03-09 23:58:53 +0700

    #6 product info skeleton

--- src/app/[lng]/products/[id]/product-id-skeleton.tsx
+++ src/app/[lng]/products/[id]/product-id-skeleton.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const ProductIdSkeleton: React.FC = () => {
-  return <div>LOADINGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG</div>
+  return (
+    <div className='flex flex-row gap-5 w-3/4 mx-auto h-[30vh]'>
+      <Skeleton containerClassName='flex-1' className='w-1/2 h-full' />
+      <Skeleton containerClassName='flex-1' className='w-1/2 h-full' />
+    </div>
+  )
 }
 
 export default ProductIdSkeleton
