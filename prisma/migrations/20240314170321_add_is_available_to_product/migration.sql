be4f4b8e76c1d553ee62be1246c5d6faa332d2ad
longbui <blong1102@gmail.com>
2024-03-15 23:54:35 +0700

    #20 fix products

---
+++ prisma/migrations/20240314170321_add_is_available_to_product/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "isAvailable" BOOLEAN NOT NULL DEFAULT false;
--- src/components/kurashi-categories/kurashi-categories.tsx
+++ src/components/kurashi-categories/kurashi-categories.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
 
-import { kurashiFetcher } from '@/utils/kurashi-fetcher'
-import { KurashiCategory } from '@/types/kurashi-category'
 import { useTranslation } from '@/i18n'
 import { KurashiTab } from '@/components/kurashi-tabs'
+
+import prisma from '@/lib/prisma'
+import { Product } from '@prisma/client'
 
 interface KurashiCategoriesProps {
   lng: string
@@ -13,23 +14,46 @@ interface KurashiCategoriesProps {
 // @ts-expect-error
 const KurashiCategories: Promise<React.JSX.Element> = async ({ lng }: KurashiCategoriesProps) => {
   const { t } = await useTranslation(lng)
-  const categories = await kurashiFetcher('http://localhost:3001') as KurashiCategory[]
-  const categoriesName = categories.map(category => category.categoryName).map(categoryName => t(categoryName))
-  const kurashiSubCategories = categories.map((category) => {
+  const productsRaw = await prisma.product.findMany({
+    take: 20,
+    where: { isAvailable: true },
+    include: { category: { select: { name: true, id: true } } }
+  })
+
+  const products = Array.from(productsRaw.reduce((acc, product): Map<string, Product[]> => {
+    if (product.category !== null) {
+      const key = product.category.name
+      if (key !== undefined && key !== '' && key !== null && acc.has(key)) {
+        const value = acc.get(key)
+        if (value !== undefined) {
+          const a = [...value, product]
+          console.log(a)
+          acc.set(key, [...value, product])
+          return acc
+        }
+      } else {
+        acc.set(key, [product])
+        return acc
+      }
+    }
+    return acc
+  }, new Map<string, Product[]>()))
+
+  const categoriesName = products.map(category => category[0]).map(categoryName => t(categoryName))
+  const productsToShow = products.map((product) => {
     return {
-      key: category.categoryName,
-      content: category.subCategories.map(sub => (
-        <Link key={sub.name} href={sub.url}>
+      key: product[0],
+      content: product[1].map((prod) => (
+        <Link key={prod.name} href={prod.url}>
           <div className='flex flex-col items-center'>
-            <img className='w-64' src={sub.thumbnail} alt='product thumbnail' />
-            <div className='mt-3 hover:cursor-pointer font-semibold hover:text-main'>{t(sub.name)}</div>
+            <img className='w-64' src={prod.thumbnail} alt='product thumbnail' />
+            <div className='mt-3 hover:cursor-pointer font-semibold hover:text-main'>{t(prod.name)}</div>
           </div>
-        </Link>
-      ))
+        </Link>))
     }
   })
 
-  return <KurashiTab body={kurashiSubCategories} tabList={categoriesName} />
+  return <KurashiTab body={productsToShow} tabList={categoriesName} />
 }
 
 export default KurashiCategories
