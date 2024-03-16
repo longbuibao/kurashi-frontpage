cadaf485b7f93d597070dcea8ede24c082f830a2
longbui <blong1102@gmail.com>
2024-03-16 19:10:41 +0700

    #6 product skeleton

--- src/app/[lng]/products/[id]/product-id-skeleton.tsx
+++ src/app/[lng]/products/[id]/product-id-skeleton.tsx
@@ -4,10 +4,10 @@ import 'react-loading-skeleton/dist/skeleton.css'
 
 const ProductIdSkeleton: React.FC = () => {
   return (
-    <div className='flex flex-col items-center w-4/5 mx-auto my-10'>
+    <div className='flex flex-col items-center w-3/4 mx-auto my-10'>
       <div className='flex flex-row gap-5 w-full h-[50vh]'>
-        <Skeleton containerClassName='flex-1' className='w-1/2 h-full' />
-        <Skeleton count={4} containerClassName='flex-1' className='w-1/2 h-8' />
+        <Skeleton containerClassName='flex-1' className='w-1/2 h-72' />
+        <Skeleton count={4} containerClassName='flex-1' className='w-1/2 h-16' />
       </div>
     </div>
 
