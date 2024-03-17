95b38a160f6b83f1dbc49a6d0e0c21004b63a43c
longbui <blong1102@gmail.com>
2024-03-17 22:44:43 +0700

    #6 skeleton

--- src/app/[lng]/products/[id]/product-id-skeleton.tsx
+++ src/app/[lng]/products/[id]/product-id-skeleton.tsx
@@ -4,13 +4,22 @@ import 'react-loading-skeleton/dist/skeleton.css'
 
 const ProductIdSkeleton: React.FC = () => {
   return (
-    <div className='flex flex-col items-center w-3/4 mx-auto my-10'>
-      <div className='flex flex-row gap-5 w-full h-[50vh]'>
-        <Skeleton containerClassName='flex-1' className='w-1/2 h-72' />
-        <Skeleton count={4} containerClassName='flex-1' className='w-1/2 h-16' />
+    <div className='flex flex-col items-center w-3/4 mx-auto my-10 h-screen'>
+      <div className='flex flex-row gap-5 w-full flex-1'>
+        <Skeleton containerClassName='flex-1' className='w-1/2 h-full' />
+        <div className='flex flex-col flex-1'>
+          <Skeleton containerClassName='flex-1' className='w-1/2 h-16' />
+          <Skeleton containerClassName='flex-1' className='w-1/2 h-16' />
+          <Skeleton containerClassName='flex-1' className='w-1/2 h-16' />
+          <Skeleton containerClassName='flex-1' className='w-1/2 h-16' />
+        </div>
+      </div>
+      <div className='w-full flex flex-col h-full my-10 gap-10'>
+        <Skeleton containerClassName='flex-1' className='w-1/2 h-full' />
+        <Skeleton containerClassName='flex-1' className='w-1/2 h-full' />
+        <Skeleton containerClassName='flex-1' className='w-1/2 h-full' />
       </div>
     </div>
-
   )
 }
 
