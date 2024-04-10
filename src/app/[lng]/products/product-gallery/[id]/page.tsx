80f419e8de81570b7e3f98754815f9cc3ced6042
longbui <blong1102@gmail.com>
2024-04-11 00:09:45 +0700

    image gallery

--- src/app/[lng]/products/product-gallery/[id]/page.tsx
+++ src/app/[lng]/products/product-gallery/[id]/page.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import dynamic from 'next/dynamic'
+
+const ImageGallery = dynamic(async () => (await import('@/components/image-gallery')).ImageGallery, { ssr: false })
+
+export const metadata = {
+  title: 'Thư viện hình ảnh'
+}
 
 const ProductGallery: React.FC = () => {
-  return <div />
+  return (
+    <div className='w-4/5 mx-auto my-10'>
+      <ImageGallery />
+    </div>
+  )
 }
 
 export default ProductGallery
