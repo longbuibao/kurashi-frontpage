1908829dde92559812001eed31be48b0c6a9b461
long bui bao <blogn1102@gmail.com>
2024-10-06 13:28:47 +0700

    fix revert

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -5,10 +5,12 @@ interface LogoProps {
   width: number
   height?: number
   srcImage?: string
+  isMxAuto?: boolean
 }
 
-const Logo: FC<LogoProps> = ({ width, height = -1, srcImage = 'https://storage.googleapis.com/kurashi_frontpage_files/images/LOGO.png' }) => {
-  return <Image width={width} height={height} src={srcImage} alt='Kurashi logo' priority />
+const Logo: FC<LogoProps> = ({ isMxAuto = false, width, height = -1, srcImage = 'https://storage.googleapis.com/kurashi_frontpage_files/images/LOGO.png' }) => {
+  const className = isMxAuto ? 'mx-auto' : ''
+  return <Image className={className} width={width} height={height} src={srcImage} alt='Kurashi logo' priority />
 }
 
 export default Logo
