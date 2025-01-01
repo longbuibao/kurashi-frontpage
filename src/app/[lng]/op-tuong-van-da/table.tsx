30f942a64ea5967748ae3f3fab1122082c112ee3
longbui <blong1102@gmail.com>
2025-01-01 19:07:46 +0700

    fix table

--- src/app/[lng]/op-tuong-van-da/table.tsx
+++ src/app/[lng]/op-tuong-van-da/table.tsx
@@ -9,7 +9,7 @@ const SpecTable: React.FC = () => {
         <thead>
           <tr>
             <th rowSpan={2}>Quy cách</th>
-            <th rowSpan={1} className='text-nowrap'>Kích thước<br /><small>Độ dày 3mm</small></th>
+            <th rowSpan={1} className='text-nowrap'>Kích thước<br /><small className='font-extrabold'>Độ dày 3mm</small></th>
             <th colSpan={1}>900×600</th>
             <th colSpan={1}>600×300</th>
             <th>900×2400</th>
@@ -29,77 +29,77 @@ const SpecTable: React.FC = () => {
           <tr>
             <td rowSpan={8}>Mã màu</td>
             <td className='cell-image'><Image width={170} height={70} src='https://storage.googleapis.com/kurashi_frontpage_files/images/tam-op-da/table/xam-traverine.png' alt='Xám travertine' /></td>
-            <td>○</td>
-            <td>○</td>
-            <td>×</td>
-            <td>○</td>
-            <td>○</td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-xmark' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
           </tr>
           <tr>
             <td className='cell-image'><Image width={170} height={70} src='https://storage.googleapis.com/kurashi_frontpage_files/images/tam-op-da/table/den-traverine.png' alt='Đen travertine' /></td>
-            <td>○</td>
-            <td>○</td>
-            <td>×</td>
-            <td>○</td>
-            <td>○</td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-xmark' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
           </tr>
           <tr>
             <td className='cell-image'><Image width={170} height={70} src='https://storage.googleapis.com/kurashi_frontpage_files/images/tam-op-da/table/be-slate.png' alt='Be slate' /></td>
-            <td>○</td>
-            <td>○</td>
-            <td>○</td>
-            <td>○</td>
-            <td>○</td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
           </tr>
           <tr>
             <td className='cell-image'><Image width={170} height={70} src='https://storage.googleapis.com/kurashi_frontpage_files/images/tam-op-da/table/den-slate.png' alt='Đen slate' /></td>
-            <td>○</td>
-            <td>○</td>
-            <td>○</td>
-            <td>○</td>
-            <td>○</td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
           </tr>
           <tr>
             <td className='cell-image'><Image width={170} height={70} src='https://storage.googleapis.com/kurashi_frontpage_files/images/tam-op-da/table/gi-sat.png' alt='Gỉ sắt' /></td>
-            <td>×</td>
-            <td>×</td>
-            <td>○</td>
-            <td>○</td>
-            <td>×</td>
+            <td><i className='fa-solid fa-xmark' /></td>
+            <td><i className='fa-solid fa-xmark' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-xmark' /></td>
           </tr>
           <tr>
             <td className='cell-image'><Image width={170} height={70} src='https://storage.googleapis.com/kurashi_frontpage_files/images/tam-op-da/table/gi-den.png' alt='Gỉ đen' /></td>
-            <td>×</td>
-            <td>×</td>
-            <td>○</td>
-            <td>○</td>
-            <td>×</td>
+            <td><i className='fa-solid fa-xmark' /></td>
+            <td><i className='fa-solid fa-xmark' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-xmark' /></td>
           </tr>
           <tr>
             <td className='cell-image'><Image width={170} height={70} src='https://storage.googleapis.com/kurashi_frontpage_files/images/tam-op-da/table/xam-xi-mang.png' alt='Xám xi măng' /></td>
-            <td>○</td>
-            <td>○</td>
-            <td>○</td>
-            <td>○</td>
-            <td>×</td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-xmark' /></td>
           </tr>
           <tr>
             <td className='cell-image'><Image width={170} height={70} src='https://storage.googleapis.com/kurashi_frontpage_files/images/tam-op-da/table/den-xi-mang.png' alt='Đen xi măng' /></td>
-            <td>○</td>
-            <td>○</td>
-            <td>○</td>
-            <td>○</td>
-            <td>×</td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-o' /></td>
+            <td><i className='fa-solid fa-xmark' /></td>
           </tr>
         </tbody>
       </table>
       <div className='self-end'>
         <div className='flex flex-row gap-2'>
-          <div>○</div>
+          <div><i className='fa-solid fa-o' /></div>
           <div>Có sản xuất</div>
         </div>
         <div className='flex flex-row gap-2'>
-          <div>×</div>
+          <div><i className='fa-solid fa-xmark' /></div>
           <div>Không sản xuất</div>
         </div>
       </div>
