7b34114f13f67265e727989c034ae7c4a7ca8052
long bui bao <blong1102@gmail.com>
2025-07-21 21:21:44 +0700

    change image

--- src/components/story-slider/story-slider.tsx
+++ src/components/story-slider/story-slider.tsx
@@ -11,31 +11,37 @@ const StorySlider: React.FC = () => {
   const [currentStoryImage, setCurrentStoryImage] = useState('https://storage.googleapis.com/kurashi_frontpage_files/images/sliders-homepage/Slider_1-01.webp')
   const storyData = [
     {
-      topImage: 'https://storage.googleapis.com/kurashi_frontpage_files/images/sliders-homepage/Slider_1-01.webp',
-      title: 'Thép Tráng Men',
+      topImage: 'https://storage.googleapis.com/kurashi_frontpage_files/images/thep_trang_men_slider/Slider01.webp',
+      thumbnail: 'https://storage.googleapis.com/kurashi_frontpage_files/images/thep_trang_men_slider/Slider01_thumb.webp',
+      title: 'THÉP TRÁNG MEN ỐP BẾP',
+      button: 'Khám phá công nghệ vật liệu',
       link: '/'
     },
     {
-      topImage: 'https://storage.googleapis.com/kurashi_frontpage_files/images/sliders-homepage/Slider%2003-01-01.webp',
-      title: 'Phụ kiện bếp nam châm',
+      topImage: 'https://storage.googleapis.com/kurashi_frontpage_files/images/thep_trang_men_slider/Slider02.webp',
+      thumbnail: 'https://storage.googleapis.com/kurashi_frontpage_files/images/thep_trang_men_slider/Slider02_thumb.webp',
+      title: 'PHỤ KIỆN BẾP',
+      button: 'Tìm hiểu bộ giải pháp',
       link: '/'
     },
     {
-      topImage: 'https://storage.googleapis.com/kurashi_frontpage_files/images/sliders-homepage/Slider%202-01.webp',
-      title: 'Tấm ốp tường vân đá',
+      topImage: 'https://storage.googleapis.com/kurashi_frontpage_files/images/thep_trang_men_slider/Slider03.webp',
+      thumbnail: 'https://storage.googleapis.com/kurashi_frontpage_files/images/thep_trang_men_slider/Slider03_thumb.webp',
+      title: 'ỐP TƯỜNG VÂN ĐÁ',
+      button: 'Khám phá bộ sưu tập',
       link: '/'
     }
   ]
-  const stories = storyData.map(({ topImage, title, link }) => ({
+  const stories = storyData.map(({ topImage, title, link, thumbnail, button }) => ({
     topImage,
     content: () => (
       <div className='w-full flex flex-row ml-10 gap-20'>
-        <Image src={topImage} width={200} height={106} alt={title} />
+        <Image src={thumbnail} width={200} height={106} alt={title} />
         <div className='flex flex-col gap-10'>
-          <div className='text-3xl'>{title}</div>
+          <div className='text-2xl'>{title}</div>
           <div className='bg-main text-secondary w-fit p-3'>
             <Link href={link as any as UrlObject} className='font-bold'>
-              Khám phá ngay
+              {button}
             </Link>
           </div>
         </div>
@@ -69,7 +75,7 @@ const StorySlider: React.FC = () => {
           sizes='100vw'
         />
       </div>
-      <div ref={containerRef} className='absolute bottom-3 left-3'>
+      <div ref={containerRef} className='absolute bottom-3 left-1/2 -translate-x-1/2'>
         <Suspense>
           <ReactInstaStories
             preloadCount={3}
@@ -77,7 +83,7 @@ const StorySlider: React.FC = () => {
             keyboardNavigation
             width={dimensions.width}
             height={dimensions.height}
-            defaultInterval={3000}
+            defaultInterval={5000}
             stories={stories}
             onStoryStart={(s: any, st: any) => setCurrentStoryImage(st.topImage)}
             storyContainerStyles={{ background: 'white', borderRadius: 8 }}
