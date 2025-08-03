cc58d6053e67eacd107c6d365027c447713b6e32
long bui bao <blong1102@gmail.com>
2025-08-03 11:10:24 +0700

    change image

--- src/components/product/product-card.tsx
+++ src/components/product/product-card.tsx
@@ -10,11 +10,11 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ product, lng }) => {
   return (
-    <div className='product-card--hover flex flex-col items-center rounded-md max-sm:w-full relative h-full'>
+    <div className='product-card--hover group flex flex-col items-center rounded-md max-sm:w-full relative h-full'>
       <div className='rounded-md bg-secondary overflow-hidden z-10 h-full'>
         <Image className='transform transition-transform duration-500 h-full' src={product.thumbnail ?? '#'} alt='product thumbnail' width={640} height={360} />
       </div>
-      <div className='z-10 flex flex-col justify-center transition rounded-md hover:cursor-pointer hover:bg-opacity-0 bg-kurashi-black absolute text-center w-full h-full'>
+      <div className='z-10 self-end flex flex-col h-10 bottom-0 justify-center transition-all duration-200 ease-in-out rounded-md hover:cursor-pointer bg-kurashi-black absolute text-center w-full group-hover:h-full'>
         <div className='font-bold text-2xl max-md:text-base text-secondary'>
           {product.name?.toUpperCase() ?? '#'}
         </div>
