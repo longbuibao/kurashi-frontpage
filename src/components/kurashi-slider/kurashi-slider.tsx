c2a3af6343bb9156c81a1f76e3a4e0e51b826720
long bui bao <blong1102@gmail.com>
2025-10-05 09:41:48 +0700

    add text

--- src/components/kurashi-slider/kurashi-slider.tsx
+++ src/components/kurashi-slider/kurashi-slider.tsx
@@ -4,9 +4,9 @@ import React, { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
 
 const steps = [
-  { thumbnail: 'https://storage.googleapis.com/kurashi_frontpage_files/images/voi-rua/thumb_feature_1.png', label: 'TÍCH HỢP PHỤ KIỆN', title: 'PHỤ KIỆN NAM CHÂM, KHÔNG CẦN KHOAN', content: 'Tấm ốp tường hút nam châm cho phép gắn, tháo và di chuyển phụ kiện từ tính tự do mà không cần khoan, giúp bề mặt tường luôn nguyên vẹn.' },
-  { thumbnail: 'https://storage.googleapis.com/kurashi_frontpage_files/images/voi-rua/thumb_feature_1.png', label: 'CHỐNG Ố', title: 'KHÓ BÁM BẨN VÀ DỄ VỆ SINH', content: 'Bề mặt hard coating chống bám dầu mỡ và bụi bẩn, giúp lau chùi nhanh chóng, đặc biệt phù hợp cho khu bếp thường xuyên có nhiều vết bẩn.' },
-  { thumbnail: 'https://storage.googleapis.com/kurashi_frontpage_files/images/voi-rua/thumb_feature_1.png', label: 'DỄ THI CÔNG', title: 'DỄ CẮT GHÉP VÀ THI CÔNG NHANH CHÓNG', content: 'Tấm ốp tường hút nam châm có trọng lượng nhẹ hơn nhiều so với ốp đá tự nhiên, dễ cắt ghép và thi công, giúp giảm đáng kể chi phí nhân công và thời gian lắp đặt.' }
+  { summary: 'Tích hợp phụ kiện', thumbnail: 'https://storage.googleapis.com/kurashi_frontpage_files/images/voi-rua/thumb_feature_1.png', label: 'TÍCH HỢP PHỤ KIỆN', title: 'PHỤ KIỆN NAM CHÂM, KHÔNG CẦN KHOAN', content: 'Tấm ốp tường hút nam châm cho phép gắn, tháo và di chuyển phụ kiện từ tính tự do mà không cần khoan, giúp bề mặt tường luôn nguyên vẹn.' },
+  { summary: 'chống ố', thumbnail: 'https://storage.googleapis.com/kurashi_frontpage_files/images/voi-rua/thumb_feature_1.png', label: 'CHỐNG Ố', title: 'KHÓ BÁM BẨN VÀ DỄ VỆ SINH', content: 'Bề mặt hard coating chống bám dầu mỡ và bụi bẩn, giúp lau chùi nhanh chóng, đặc biệt phù hợp cho khu bếp thường xuyên có nhiều vết bẩn.' },
+  { summary: 'Dễ thi công', thumbnail: 'https://storage.googleapis.com/kurashi_frontpage_files/images/voi-rua/thumb_feature_1.png', label: 'DỄ THI CÔNG', title: 'DỄ CẮT GHÉP VÀ THI CÔNG NHANH CHÓNG', content: 'Tấm ốp tường hút nam châm có trọng lượng nhẹ hơn nhiều so với ốp đá tự nhiên, dễ cắt ghép và thi công, giúp giảm đáng kể chi phí nhân công và thời gian lắp đặt.' }
 ]
 
 const KurashiSlider = (): React.ReactElement => {
@@ -48,19 +48,23 @@ const KurashiSlider = (): React.ReactElement => {
           </div>
         </div>
       </div>
-      <div className='w-full bg-secondary h-[1px] relative -mt-10]'>
-        <div className='w-4/5 mx-auto flex flex-row justify-between absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2'>
+      <div className='w-full bg-secondary h-[1px] relative'>
+        <div className='w-4/5 mx-auto flex justify-between items-center -translate-y-[1.125rem]'>
           {steps.map((x, y) => {
             const isActive = index === y
             const className = `size-9 rounded-full transition-colors ${isActive ? 'bg-main text-secondary' : 'bg-secondary border-black'}`
             return (
-              <button onClick={() => handleClick(y)} key={x.title} className={className}>
-                <i className='fa-solid fa-chevron-right' />
-              </button>
+              <div key={x.title} className='flex flex-col items-center gap-5'>
+                <button onClick={() => handleClick(y)} className={className}>
+                  <i className='fa-solid fa-chevron-right' />
+                </button>
+                {x.summary.toUpperCase()}
+              </div>
             )
           })}
         </div>
       </div>
+
     </div>
 
   )
